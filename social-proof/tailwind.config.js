164ca358bb15d6fe9f6b0aa547dbfd0386e445ec
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-04-03 20:52:54 +0100

    Remove unused color variable

--- social-proof/tailwind.config.js
+++ social-proof/tailwind.config.js
@@ -6,11 +6,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "custom-red": "#EA5454",
-        "custom-light-blue": "#44D3D2",
-        "custom-orange": "#FCAE4A",
-        "custom-blue": "#549EF2",
-        "custom-gray": "#4D4F62",
       },
     },
   },
