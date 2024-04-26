e468f45a1cb7178ba71ec60cd93b681e7ef43b4c
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-04-26 15:58:04 +0100

    Update styles.css and component templates

--- audiophile-ecommerce-website/src/app/app.component.ts
+++ audiophile-ecommerce-website/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CategoryComponent } from './category/category.component';
+import { AudioGearTextComponent } from './audio-gear-text/audio-gear-text.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, CategoryComponent],
+  imports: [RouterOutlet, NavBarComponent, CategoryComponent, AudioGearTextComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
